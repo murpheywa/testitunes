240f4f44d5d77d5e0213cf0014f4f7049225d784
kevinmurphey <kmurphey@blinkx.com>
2016-09-11 11:21:53 -0700

    Initial commit

---
+++ TestITunes/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace TestITunes
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmdRefresh = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tbSrc = new System.Windows.Forms.RichTextBox();
+			this.tbDst = new System.Windows.Forms.RichTextBox();
+			this.cmdExport = new System.Windows.Forms.Button();
+			this.ssStatue = new System.Windows.Forms.StatusStrip();
+			this.tslPlaylist = new System.Windows.Forms.ToolStripStatusLabel();
+			this.tslTrack = new System.Windows.Forms.ToolStripStatusLabel();
+			this.tslState = new System.Windows.Forms.ToolStripStatusLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtTarget = new System.Windows.Forms.TextBox();
+			this.cmdcancel = new System.Windows.Forms.Button();
+			this.chkOverwrite = new System.Windows.Forms.CheckBox();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.txtLog = new System.Windows.Forms.RichTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.ssStatue.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// cmdRefresh
+			// 
+			this.cmdRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdRefresh.Location = new System.Drawing.Point(681, 12);
+			this.cmdRefresh.Name = "cmdRefresh";
+			this.cmdRefresh.Size = new System.Drawing.Size(83, 36);
+			this.cmdRefresh.TabIndex = 0;
+			this.cmdRefresh.Text = "Refresh";
+			this.cmdRefresh.UseVisualStyleBackColor = true;
+			this.cmdRefresh.Click += new System.EventHandler(this.cmdRefresh_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tbSrc);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tbDst);
+			this.splitContainer1.Size = new System.Drawing.Size(663, 328);
+			this.splitContainer1.SplitterDistance = 340;
+			this.splitContainer1.TabIndex = 1;
+			// 
+			// tbSrc
+			// 
+			this.tbSrc.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbSrc.Location = new System.Drawing.Point(0, 0);
+			this.tbSrc.Name = "tbSrc";
+			this.tbSrc.ReadOnly = true;
+			this.tbSrc.Size = new System.Drawing.Size(340, 328);
+			this.tbSrc.TabIndex = 0;
+			this.tbSrc.Text = "";
+			// 
+			// tbDst
+			// 
+			this.tbDst.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbDst.Location = new System.Drawing.Point(0, 0);
+			this.tbDst.Name = "tbDst";
+			this.tbDst.Size = new System.Drawing.Size(319, 328);
+			this.tbDst.TabIndex = 0;
+			this.tbDst.Text = "";
+			// 
+			// cmdExport
+			// 
+			this.cmdExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdExport.Location = new System.Drawing.Point(681, 63);
+			this.cmdExport.Name = "cmdExport";
+			this.cmdExport.Size = new System.Drawing.Size(83, 36);
+			this.cmdExport.TabIndex = 1;
+			this.cmdExport.Text = "Export";
+			this.cmdExport.UseVisualStyleBackColor = true;
+			this.cmdExport.Click += new System.EventHandler(this.cmdExport_Click);
+			// 
+			// ssStatue
+			// 
+			this.ssStatue.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslPlaylist,
+            this.tslTrack,
+            this.tslState});
+			this.ssStatue.Location = new System.Drawing.Point(0, 438);
+			this.ssStatue.Name = "ssStatue";
+			this.ssStatue.Size = new System.Drawing.Size(776, 22);
+			this.ssStatue.TabIndex = 2;
+			this.ssStatue.Text = "statusStrip1";
+			// 
+			// tslPlaylist
+			// 
+			this.tslPlaylist.AutoSize = false;
+			this.tslPlaylist.Name = "tslPlaylist";
+			this.tslPlaylist.Size = new System.Drawing.Size(350, 17);
+			// 
+			// tslTrack
+			// 
+			this.tslTrack.Name = "tslTrack";
+			this.tslTrack.Size = new System.Drawing.Size(360, 17);
+			this.tslTrack.Spring = true;
+			// 
+			// tslState
+			// 
+			this.tslState.Name = "tslState";
+			this.tslState.Size = new System.Drawing.Size(51, 17);
+			this.tslState.Text = "Stopped";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Target Directory";
+			// 
+			// txtTarget
+			// 
+			this.txtTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtTarget.Location = new System.Drawing.Point(101, 6);
+			this.txtTarget.Name = "txtTarget";
+			this.txtTarget.Size = new System.Drawing.Size(574, 20);
+			this.txtTarget.TabIndex = 4;
+			// 
+			// cmdcancel
+			// 
+			this.cmdcancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdcancel.Location = new System.Drawing.Point(681, 118);
+			this.cmdcancel.Name = "cmdcancel";
+			this.cmdcancel.Size = new System.Drawing.Size(83, 36);
+			this.cmdcancel.TabIndex = 5;
+			this.cmdcancel.Text = "Cancel";
+			this.cmdcancel.UseVisualStyleBackColor = true;
+			this.cmdcancel.Click += new System.EventHandler(this.cmdcancel_Click);
+			// 
+			// chkOverwrite
+			// 
+			this.chkOverwrite.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.chkOverwrite.AutoSize = true;
+			this.chkOverwrite.Location = new System.Drawing.Point(693, 181);
+			this.chkOverwrite.Name = "chkOverwrite";
+			this.chkOverwrite.Size = new System.Drawing.Size(71, 17);
+			this.chkOverwrite.TabIndex = 6;
+			this.chkOverwrite.Text = "Overwrite";
+			this.chkOverwrite.UseVisualStyleBackColor = true;
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer2.Location = new System.Drawing.Point(12, 32);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.txtLog);
+			this.splitContainer2.Size = new System.Drawing.Size(663, 403);
+			this.splitContainer2.SplitterDistance = 328;
+			this.splitContainer2.TabIndex = 7;
+			// 
+			// txtLog
+			// 
+			this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtLog.Location = new System.Drawing.Point(0, 0);
+			this.txtLog.Name = "txtLog";
+			this.txtLog.Size = new System.Drawing.Size(663, 71);
+			this.txtLog.TabIndex = 1;
+			this.txtLog.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(776, 460);
+			this.Controls.Add(this.splitContainer2);
+			this.Controls.Add(this.chkOverwrite);
+			this.Controls.Add(this.cmdcancel);
+			this.Controls.Add(this.txtTarget);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.ssStatue);
+			this.Controls.Add(this.cmdExport);
+			this.Controls.Add(this.cmdRefresh);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ssStatue.ResumeLayout(false);
+			this.ssStatue.PerformLayout();
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button cmdRefresh;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.RichTextBox tbSrc;
+		private System.Windows.Forms.RichTextBox tbDst;
+		private System.Windows.Forms.Button cmdExport;
+		private System.Windows.Forms.StatusStrip ssStatue;
+		private System.Windows.Forms.ToolStripStatusLabel tslPlaylist;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtTarget;
+		private System.Windows.Forms.Button cmdcancel;
+		private System.Windows.Forms.ToolStripStatusLabel tslTrack;
+		private System.Windows.Forms.CheckBox chkOverwrite;
+		private System.Windows.Forms.ToolStripStatusLabel tslState;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.RichTextBox txtLog;
+	}
+}
+
---
+++ TestITunes/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTunesLib;
+using System.Diagnostics;
+using TestITunes.Properties;
+using System.IO;
+
+
+namespace TestITunes
+{
+	public partial class Form1 : Form
+	{
+		delegate void Router(object arg);
+		iTunesApp _app = null;
+		Dictionary<string, UInt64> _playListtDict = null;
+		bool _bCancel = false;
+		private string _sPlayListName;
+		IITUserPlaylist _playList = null;
+		IITFileOrCDTrack _track = null;
+		string _sTargetDir = "";
+		private bool _bRefreshed = false;
+		string _sPlayListTargetPath = "";
+		string _sTrackTargetPath = "";
+		string _sSrcPath = "";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			_playListtDict = new Dictionary<string, UInt64>(StringComparer.OrdinalIgnoreCase);
+			GetSettings();
+			tbDst.SelectionStart = 0;
+			tbDst.ScrollToCaret();
+			tslPlaylist.Text = ""; tslTrack.Text = "";
+			tslState.Text = "Stopped";
+			_app = new iTunesApp();
+		}
+
+		void Alert(string sText)
+		{
+			MessageBox.Show(sText);
+		}
+
+		private void GetSettings()
+		{
+			tbDst.Text = Settings.Default.Dst;
+			txtTarget.Text = Settings.Default.TargetDir;
+
+			bool bChecked = false;
+			Boolean.TryParse(Settings.Default.Overwrite, out bChecked);
+			chkOverwrite.Checked = bChecked;
+        }
+
+		private void SaveSettings()
+		{
+			Settings.Default.Dst = tbDst.Text;
+			Settings.Default.TargetDir = txtTarget.Text;
+			Settings.Default.Overwrite = chkOverwrite.Checked.ToString();
+			Settings.Default.Save();
+		}
+		string NameToFileName(string sName, string sExtraInvalidChars)
+		{
+			string sInvalids = new string(Path.GetInvalidFileNameChars()) + sExtraInvalidChars;
+			StringBuilder bldrPath = new StringBuilder();
+			foreach( char iterCh in sName)
+			{
+				if (sInvalids.IndexOf(iterCh) >= 0 )
+				{
+					bldrPath.Append('_');
+				}
+				else
+				{
+					bldrPath.Append(iterCh);
+				}
+			}
+
+			return bldrPath.ToString();
+		}
+
+		string NameToFileName(string sName)
+		{
+			return NameToFileName(sName, "");
+		}
+
+		private string TrackNameToPath(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (_app != null)
+			{
+				_app = null;
+			}
+			SaveSettings();
+		}
+
+		private void cmdRefresh_Click(object sender, EventArgs e)
+		{
+			if (_app == null)
+			{
+				_app = new iTunesApp();
+			}
+			tslPlaylist.Text = ""; tslTrack.Text = "";
+			string sVersion = _app.Version;
+			string sPath = _app.LibraryXMLPath;
+
+			tbSrc.Clear();
+			var oSource = _app.LibrarySource;
+            foreach (IITPlaylist pl in oSource.Playlists)
+			{
+				ITPlaylistKind eKind = pl.Kind;
+				IITUserPlaylist plUser = null;
+				bool bSmart = false;
+				ITUserPlaylistSpecialKind eSpecialKind = ITUserPlaylistSpecialKind.ITUserPlaylistSpecialKindNone;
+				IITUserPlaylist u2 = null;
+				if ( eKind == ITPlaylistKind.ITPlaylistKindUser)
+				{
+					plUser = pl as IITUserPlaylist;
+					bSmart = plUser.Smart;
+					eSpecialKind = plUser.SpecialKind;
+					if ( bSmart == false && eSpecialKind == ITUserPlaylistSpecialKind.ITUserPlaylistSpecialKindNone)
+					{
+						UInt64 uIndex = (UInt64)(((long)plUser.sourceID) << 32 | (long)plUser.playlistID);
+						_playListtDict.Add(plUser.Name, uIndex);
+						// confirm
+						u2 = GetPlayList(plUser.Name);
+
+                    }
+				}
+				tbSrc.SelectionStart = 0;
+				tbSrc.ScrollToCaret();
+
+				_bRefreshed = true;
+			}
+
+			foreach (KeyValuePair<string,UInt64> iterPair in _playListtDict.OrderBy(x => x.Key.ToLower()))
+            {
+				tbSrc.AppendText(iterPair.Key + Environment.NewLine);
+			}
+
+		}
+
+		private IITUserPlaylist GetPlayList(string name)
+		{
+			IITUserPlaylist pl = null;
+			UInt64 uiSourcePlaylist = 0;
+			if (_playListtDict.TryGetValue(name, out uiSourcePlaylist))
+			{
+				int uiSource = (int)(uiSourcePlaylist >> 32);
+				int uiPlaylist = (int)(uiSourcePlaylist & 0x00000000ffffffff);
+				IITObject o = null;
+				try
+				{
+					o = _app.GetITObjectByID(uiSource, uiPlaylist, 0, 0);
+				}
+				catch (Exception)
+				{
+				}
+				pl = o as IITUserPlaylist;
+			}
+
+			return pl;
+		}
+
+		private void cmdExport_Click(object sender, EventArgs e)
+		{
+			SaveSettings();
+			txtLog.Clear();
+
+			if (string.IsNullOrWhiteSpace(txtTarget.Text))
+			{
+				Alert("No target directory selected");
+			}
+			else if (string.IsNullOrWhiteSpace(tbDst.Text))
+			{
+				Alert("No playlists selected");
+			}
+			else if (! _bRefreshed)
+			{
+				Alert("Playlists need to be refreshed");
+			}
+			else
+			{
+				try
+				{
+					tslPlaylist.Text = ""; tslTrack.Text = "";
+					_sTargetDir = txtTarget.Text;
+					Directory.CreateDirectory(_sTargetDir);
+					_bCancel = false;
+					tslState.Text = "Running";
+					DoExport();
+					tslState.Text = 
+						_bCancel
+						? "Stopped"
+						: "Done";
+					_bCancel = false;
+				}
+				catch (Exception ex)
+				{
+					Alert(ex.Message);
+				}
+			}
+		}
+
+		private void DoExport()
+		{
+			tslPlaylist.Text = "";
+			foreach (string sIterPlayList in tbDst.Text.Replace("\r","").Split('\n'))
+			{
+				if (! string.IsNullOrWhiteSpace(sIterPlayList))
+				{
+					_sPlayListName = sIterPlayList;
+					_sPlayListTargetPath = Path.Combine(_sTargetDir, NameToFileName(_sPlayListName));
+					_playList = GetPlayList(_sPlayListName);
+					if ( _playList == null)
+					{
+						Alert("Cannot find playlist " + _sPlayListName);
+						_bCancel = true;
+						break;
+					}
+
+					Directory.CreateDirectory(_sPlayListTargetPath);
+					tslPlaylist.Text = _sPlayListName;
+					DoExportPlaylist();
+					if ( _bCancel)
+					{
+						break;
+					}
+				}
+			}
+		}
+
+		private void DoExportPlaylist()
+		{
+			foreach( IITTrack iterTrack in _playList.Tracks)
+			{
+				_track = iterTrack as IITFileOrCDTrack;
+				tslTrack.Text = _track.Name;
+				Application.DoEvents();
+				if ( _bCancel )
+				{
+					break;
+				}
+
+				_sSrcPath = _track.Location;
+				ITTrackKind kind = _track.Kind;
+				string sKind = _track.KindAsString;
+				string sTargetName = NameToFileName(_track.Name, " ");
+				// to fix version 1 bug
+				_sTrackTargetPath = Path.Combine(_sPlayListTargetPath, sTargetName);
+				if (File.Exists(_sTrackTargetPath))
+				{
+					File.Delete(_sTrackTargetPath);
+				}
+				string sSrcExt = Path.GetExtension(_sSrcPath);
+				_sTrackTargetPath = Path.Combine(_sPlayListTargetPath, sTargetName + sSrcExt);
+				bool bExists = File.Exists(_sTrackTargetPath);
+				if ((!bExists) || chkOverwrite.Checked)
+				{
+					if (sKind.IndexOf("protected", StringComparison.Ordinal) >= 0)
+					{
+						string sMsg = String.Format("{0} {1} is copy protected",
+							_sPlayListName, _track.Name);
+						Log(sMsg);
+					}
+					else
+					{
+						try
+						{
+							File.Copy(_sSrcPath, _sTrackTargetPath, true);
+						}
+						catch (Exception ex)
+						{
+							Alert(ex.Message);
+							_bCancel = true;
+							break;
+						}
+					}
+				}
+            }
+		}
+
+		private void Log(string sText)
+		{
+			txtLog.AppendText(sText + Environment.NewLine);
+		}
+
+		private void cmdcancel_Click(object sender, EventArgs e)
+		{
+			_bCancel = true;
+		}
+	}
+}
